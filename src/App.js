8d35eb9f4cf1ea835d18803b0bb23089403c348d
jeetintyagi <jt9999709701@gmail.com>
2020-08-23 14:07:19 +0530

    Component/SearchForm added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,46 @@ import useFetchJobs from "./APIs/useFetchJobs";
 import Job from "./screens/Job";
 import SkeletonLoading from "./screens/SkeletonLoading";
 import { GoMarkGithub } from "react-icons/go";
+import JobsPagination from "./Components/JobsPagination";
+import SearchForm from "./Components/SearchForm";
 
 function App() {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
 
-  const { jobs, loading, error } = useFetchJobs();
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  function handleParamChange(e) {
+    const params = e.target.param;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [params]: value };
+    });
+  }
 
   return (
     <div>
       <Container className="my-4">
-        <header className="btn">
-          <h1 className=" text-light mb-4 ">
-            <GoMarkGithub />
-            {"  "} GitJOBS
-          </h1>
-        </header>
+        {
+          <header className="btn">
+            <h1 className=" text-light mb-4 ">
+              <GoMarkGithub />
+              {"  "} GitJOBS
+            </h1>
+          </header>
+        }
+
+        <SearchForm
+          params={params}
+          onParamChange={handleParamChange}
+        />
+
+        <JobsPagination
+          page={page}
+          setPage={setPage}
+          hasNextPage={hasNextPage}
+        />
 
         {loading && (
           <>
@@ -33,6 +57,11 @@ function App() {
         {jobs.map((job) => {
           return <Job key={job.id} job={job} />;
         })}
+        <JobsPagination
+          page={page}
+          setPage={setPage}
+          hasNextPage={hasNextPage}
+        />
       </Container>
     </div>
   );
